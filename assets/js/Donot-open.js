22136dfe9612009f7cdf933a7d838973f2818a78
Ibrahim Kilani <112987450+kilaniBarhoom@users.noreply.github.com>
2023-04-04 23:24:53 +0300

    Add files via upload

--- assets/js/Donot-open.js
+++ assets/js/Donot-open.js
@@ -48,7 +48,7 @@ function reFillTask()
           <span class="tasknumber">${task.number}</span>
           <span class="taskdate"> 
   
-          ${task.date}
+          ${ TaskArray[iteration].checked? "Completed":task.date}
                
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
               <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
@@ -115,7 +115,7 @@ function reFillHiddenTask()
           <span class="tasknumber">${task.number}</span>
           <span class="taskdate"> 
   
-          ${task.date}
+          ${ HiddenTaskArray[iteration].checked? "Completed":task.date}
                 
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-calendar" viewBox="0 0 16 16">
               <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM1 4v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V4H1z"/>
